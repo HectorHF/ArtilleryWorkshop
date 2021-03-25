92774e2b244e5982f6477c8734f8bb8fa000ecaa
HectorHF <hec27795@hotmail.com>
2021-03-24 21:19:39 -0600

    Comments deleted

--- myFunctions.js
+++ myFunctions.js
@@ -9,13 +9,9 @@ const Faker = require('faker');
 
 function randomData(userContext, events, done) {
   // generate data with Faker:
-  const duration = Faker.random.number();
-  const arrivalRate = Faker.random.number();
   const content = Faker.random.word();
   const updatedContent = Faker.random.word();
   // add variables to virtual user's context:
-  userContext.vars.duration = duration;
-  userContext.vars.arrivalRate = arrivalRate;
   userContext.vars.content = content;
   userContext.vars.updatedContent = updatedContent;
   // continue with executing the scenario:
